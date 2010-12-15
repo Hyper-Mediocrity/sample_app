eb15a149f30b2bbafebebfe9a159140e1c1aaeb5
Grant Adamson <grant_adamson@me.com>
2010-12-15 00:14:12 -0800

    Imporved the user show page and added a Gravatar

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,8 @@
 module UsersHelper
+  def gravatar_for(user, options = { :size => 50 } ) # default value for options
+    gravatar_image_tag(user.email.downcase, # gravatar is case-sensitive
+                       :alt => user.name,
+                       :class => "gravatar",
+                       :gravatar => options) 
+  end
 end
